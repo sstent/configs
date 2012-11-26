135723bfd6418afafb2c354ad4f532aa5b33d6a3
sstent <stuart.stent@gmail.com>
2012-11-26 16:42:45 -0500

    hmm

---
+++ openrc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export OS_USERNAME=sstent
+export OS_TENANT_NAME=test
+export OS_TENANT_ID=29eab673891a46f8b44e78830243d2b9
+export OS_PASSWORD=sstent
+export OS_AUTH_URL=http://mysql.snjpod1:5000/v2.0/
+export OS_REGION_NAME=RegionOne
